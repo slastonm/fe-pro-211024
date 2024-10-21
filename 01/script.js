b3036a606e73c7c865ab12043de4925ed3537347
slastonm <slastonm@gmail.com>
2024-10-21 21:03:07 +0300

    01

---
+++ 01/script.js
@@ -0,0 +1,86 @@
+// alert('Hello JS');
+// var
+// Коментарі  Ctrl +/ Cmnd + /
+
+// var fruits = 'apple'; застарілий варіант
+let orangeBox = 'orange';
+const KEYVALUE = 'secreet key';
+
+
+// let my-color ='red'; бо - це знак мінус
+let my_color ='red';
+let mycolor ='black';
+let Mycolor ='black2';
+let myColor ='black3';
+let MyColor ='black4';
+// let let ='white';
+
+let letMyVallue;
+
+my_color = 'gray';
+
+
+//String 
+
+let userName = 'John';
+let UserName = "Bob";
+let userText = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, quaerat.';
+
+// Number
+
+let userAge = 101;
+let floatNumber = 1.0000002;
+let numberValue = Infinity;
+// let numberValue = -Infinity;
+// let numberValue = +Infinity;
+let nanValue = NaN;
+
+//Boolean
+
+let isAdmin = true; // 2>1 true  
+let isLoginUser = false; // 2>10 false
+
+let userPassword = 'qwerty123456';
+
+// Null
+
+let noValue = null;
+
+//Undefined
+let noFindValue = undefined;
+
+// alert('Hello');
+// alert('How');
+// alert('are');
+// alert('You?');
+
+console.log('Hello how are you?');
+console.log(userPassword);
+
+let variableInfo = `User info ${userName} user age: ${userAge} user password ${userPassword}`;
+
+console.log(variableInfo);
+userName = 'Lina';
+console.log('User name:', userName);
+
+// - Віднімання
+// + Додавання
+// * множення
+// / ділення
+// % залишок від ділення
+// ** піднесення до степеня
+
+let firstValue = 3;
+let secondValue ='2';
+let summResult = firstValue + secondValue; // '32'
+console.log(`+ operation ${summResult}`);
+console.log(`+ operation result ${firstValue} + ${secondValue} = ${firstValue+secondValue}`);
+
+
+
+
+
+
+
+
+
